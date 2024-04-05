42528aefbe9928d91f427cf48e05d121cbb6dc92
rhysj22 <rojanfield@gmail.com>
2024-04-05 13:14:10 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("Hello, my name is Samer")
 
 print("Hello, my name is Rhys Jones")
+
+print("Hello, my name is Kushal")
+
+print("Hello, my name is Kian Pierce")
+
+print("Hello, my name is Andrew")
